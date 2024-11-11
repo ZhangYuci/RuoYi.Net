55c1b94496e84fa0519b9ea7d13b813c363e4a59
ZhangYuci <yuci_zhangxp@163.com>
2024-11-11 17:27:28 +0800

    启动样式调整

--- RuoYi.Admin/Program.cs
+++ RuoYi.Admin/Program.cs
@@ -6,11 +6,20 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        const string ruoyi_format_string = "\r\n  " +
+             "_____         __     ___     _   _ ______ _______ \r\n " +
+             "|  __ \\        \\ \\   / (_)   | \\ | |  ____|__   __|\r\n " +
+             "| |__) |   _  __\\ \\_/ / _    |  \\| | |__     | |   \r\n " +
+             "|  _  / | | |/ _ \\   / | |   | . ` |  __|    | |   \r\n " +
+             "| | \\ \\ |_| | (_) | |  | |  _| |\\  | |____   | |   \r\n " +
+             "|_|  \\_\\__,_|\\___/|_|  |_| (_)_| \\_|______|  |_|   \r\n" +
+             "\r\n";
+
         var builder = WebApplication.CreateBuilder(args).Inject();
         builder.WebHost.ConfigureKestrel(serverOptions =>
         {
-            // Set properties and call methods on options
-            // serverOptions.Limits.MaxRequestBodySize = 50 * 1024 * 1024;
+            //Set properties and call methods on options
+            //serverOptions.Limits.MaxRequestBodySize = 50 * 1024 * 1024;
             //serverOptions.Listen(IPAddress.IPv6Any,5000); // 监听所有 IPv6 地址
             //serverOptions.Listen(IPAddress.Any, 5000);      // 监听所有 IPv4 地址
             serverOptions.Limits.KeepAliveTimeout = TimeSpan.FromMinutes(3);
@@ -20,6 +29,12 @@ internal class Program
         // 用AspectCore替换默认的IOC容器, 用于AOP拦截, 如 事务拦截器: TransactionalAttribute 
         builder.Host.UseServiceProviderFactory(new DynamicProxyServiceProviderFactory());
 
-        builder.Build().Run();
+        var app = builder.Build();
+
+        var logger = app.Services.GetService<ILogger<Program>>();
+
+        logger.LogInformation(ruoyi_format_string);
+
+        app.Run();
     }
 }
